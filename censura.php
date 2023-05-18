7eaf3abf111f8af1f78669f9265891a6421cd0f0
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-05-18 16:17:28 +0200

    Struttura e form base

---
+++ censura.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Censura</title>
+</head>
+<body>
+
+  <h2>Pagina censura</h2>
+<p><?php echo $_GET['sezioneParagrafo'] . ', la parola censurata è: ' . $_GET['sezioneParola']  ?></p>
+
+  
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+$nome = "Lorenzo"
+
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Intro php</title>
+</head>
+<body>
+  
+<h1>Ciao <?php echo $nome ?></h1>
+
+<form action="censura.php" method="GET">
+  <label for="">Paragrafo</label>
+  <input type="text" placeholder="Inserisci qualcosa" name="sezioneParagrafo">
+
+  <label for="">Parola da censurare</label>
+  <input type="text" placeholder="Inserisci parola" name="sezioneParola">
+
+  <button type="submit">invio</button>
+
+</form>
+
+</body>
+</html>
